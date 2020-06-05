6321c81e624a9d11aa042d918ba347521f14f5db
irawanria85 <irawanria85@gmail.com>
2020-06-05 14:23:40 +0700

    Create randomhijab.js

---
+++ randomhijab.js
@@ -0,0 +1,19 @@
+function getRandomUrl(urls) {
+var minIndex = 0;
+var maxIndex = urls.length - 0.5;
+var randomIndex = Math.floor(Math.random() * (maxIndex - minIndex)) + minIndex;
+return urls[randomIndex];
+}
+var urls = [
+
+"https://www.youtube.com/embed/RCSrh6am0BQ?autoplay=1", 
+"https://www.youtube.com/embed/nDveC3G4Uxs?autoplay=1",
+"https://www.youtube.com/embed/Rjk4YQe_Yzc?autoplay=1",
+"https://www.youtube.com/embed/Tv9XjAU9dzs?autoplay=1",
+"https://www.youtube.com/embed/iVXkFftbZNo?autoplay=1",
+"https://www.youtube.com/embed/s3mDva_Wd8U?autoplay=1"
+
+];
+var randomSelectedUrl = getRandomUrl(urls);
+$("#vitrine").html(
+"<iframe class='random-iframe' src='" + randomSelectedUrl + "' width='100%' height='100%' frameborder='0' allow="autoplay; encrypted-media" seamless='seamless'></iframe>");
